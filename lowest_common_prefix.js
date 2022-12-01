858643a4f93c6e4f362a8ce83a00f658c041374a
Mike Tagg <mdtagg91@gmail.com>
2022-12-01 13:13:28 -0800

    explanation and what i learned added to lowest common prefix

--- lowest_common_prefix.js
+++ lowest_common_prefix.js
@@ -24,3 +24,24 @@ to the first word
 The while loop is saying run until the word we are on matches prefix
 Line 9 is taking a letter off of the first word on each iteration
 When enough letters are taken off prefix matches the word were on */
+
+
+/* 
+WHAT I LEARNED 
+
+-.indexOf returns -1 if there is no match. This can be used with a 
+conditional statement in problems that require reducing something down.
+
+-while loops dont need a counting variable to terminate, a conditional statement 
+paired with a changing variable can also be used. 
+
+-if statements dont need brackets if the return is one line long
+
+The right way to look at this challenge is through the lens of reduction. 
+Ultimately we want to see what the shortest common prefix is so our return 
+value is either going to be equal to or less than the shortest word in the 
+list. This means we can set a variable to be the first entry in the list 
+and then compare each other entry against it to find a common trait. 
+Similar to finding the max or min value in a list of numbers. 
+
+*/
