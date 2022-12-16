70bb05c72f7692df519305afae07b232cfaaec46
Mike Tagg <mdtagg91@gmail.com>
2022-12-15 18:55:40 -0800

    sudoku validator solution found for boxes, broke up code into formulas

--- Hard/sudoku_validator.js
+++ Hard/sudoku_validator.js
@@ -1,40 +1,62 @@
 function validSolution(board) {
-    let rowCount = 2
-    let rows = []
+    
+    // console.log(rowCheck(board))
+    // console.log(columnCheck(board))
+    // console.log(boxCheck(board))
+
+    if(rowCheck(board) && columnCheck(board) && boxCheck(board)) {
+        // console.log(true)
+        return true
+    }else {
+        console.log(false)
+        return false
+    }
+}
+
+const rowCheck = (board) => {
+    for(let i = 0;i < board.length;i++) {
+        if(!board[i].slice().sort().join('').includes('123456789')) {
+            return false
+        }else if(i === 8) {
+            // console.log(true)
+            return true
+        }
+    }
+}
+
+const columnCheck = (board) => {
+    let count = 0
     let columns = []
-    let boxes = []
-
-    while(board[8].length > 0) {
-        let columnBox = []
-        let box = []
-
+    while(count < board.length) {
+        let column = []
         for(let i = 0;i < 9;i++) {
-            columnBox.push(board[i][rowCount])
-            if(rows.length < 9) {
-               rows.push(board[i].slice(0,board[i].length))
-            }
+            column.push(board[i][count]) 
             if(i === 8) {
-                columns.push(columnBox)
-                columnBox = []
+                columns.push(column)
             }
-            
-            if(rowCount === 0) {
-                box.push(...board[i].splice(0,3))
-            }
-            if(rowCount === 0 && (i === 2 || i === 5 || i === 8)) {
+        }
+        count++
+    }
+    // console.log(columns)
+    return rowCheck(columns)
+}
+
+const boxCheck = (board) => {
+    let count = 0
+    let boxes = []
+    while(count <= 6) {
+        let box = []
+        for(let i = 0;i < board.length;i++) {
+            box.push(...board[i].splice(0,3))
+            if(i === 2 || i === 5 || i === 8) {
                 boxes.push(box)
                 box = []
             }
-            if(rowCount === 0 && i === 8) {
-                rowCount += 2
-            }
-            
         }
-        rowCount--
+        count+=3
     }
-    console.log(columns)
-    console.log(boxes)
-    console.log(rows)
+    // console.log(boxes)
+    return rowCheck(boxes)
 }
 
 validSolution([[5, 3, 4, 6, 7, 8, 9, 1, 2], 
@@ -46,3 +68,41 @@ validSolution([[5, 3, 4, 6, 7, 8, 9, 1, 2],
                 [9, 6, 1, 5, 3, 7, 2, 8, 4],
                 [2, 8, 7, 4, 1, 9, 6, 3, 5],
                 [3, 4, 5, 2, 8, 6, 1, 7, 9]])
+
+
+                // let rowCount = 2
+                // let rows = []
+                // let columns = []
+                // let boxes = []
+            
+                // while(board[8].length > 0) {
+                //     let columnBox = []
+                //     let box = []
+            
+                //     for(let i = 0;i < 9;i++) {
+                //         columnBox.push(board[i][rowCount])
+                //         if(rows.length < 9) {
+                //            rows.push(board[i].slice(0,board[i].length))
+                //         }
+                //         if(i === 8) {
+                //             columns.push(columnBox)
+                //             columnBox = []
+                //         }
+                        
+                //         if(rowCount === 0) {
+                //             box.push(...board[i].splice(0,3))
+                //         }
+                //         if(rowCount === 0 && (i === 2 || i === 5 || i === 8)) {
+                //             boxes.push(box)
+                //             box = []
+                //         }
+                //         if(rowCount === 0 && i === 8) {
+                //             rowCount += 2
+                //         }
+                        
+                //     }
+                //     rowCount--
+                // }
+                // console.log(columns)
+                // console.log(boxes)
+                // console.log(rows)
