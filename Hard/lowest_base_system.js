a0493179c382c9e504e9093655ab76ffd51c2b5e
Mike Tagg <mdtagg91@gmail.com>
2022-12-16 22:33:06 -0800

    more efficient solution added but only working for numbers without complex digits in specific places

--- Hard/lowest_base_system.js
+++ Hard/lowest_base_system.js
@@ -1,41 +1,81 @@
 
 function lowestBase(s) {
-    let number = s
-    let base = 2
-    console.log(s.toString(20))
-    while(number !== 0) {
-        let baseNumbers = [1]
-        let currentBase = base
-        let baseRep = []
-        while(base < number) {
-            baseNumbers.push(base)
-            base *= currentBase
+    let numbers = []
+    for(let i = 2;i < 11;i++) {
+        if((s-1) % i === 0) {
+            console.log(i)
+            numbers.push(i)
+        }else if(!numbers.length) {
+            console.log(s - 1)
+            break
         }
-        // console.log(baseNumbers)
-        for(let i = baseNumbers.length - 1;i >= 0;i--) {
-            
-            if(number - baseNumbers[i] >= 0) {
-                number -= baseNumbers[i]
-                baseRep.push('1')
-            }else {
-                baseRep.push('0')
+        
+   }
+   for(let i = numbers.length - 1;i >= 0;i--) {
+        let base = numbers[i]
+        let prevBase = numbers[i]
+        while(base <= s) {
+            base *= base
+            base += prevBase
+            prevBase = base
+            // console.log(base)
+            if(base === s - 1) {
+                console.log(numbers[i])
+                break
             }
-            if(i === 0 && number !== 0) {
-                baseRep.push('0')
-            }
         }
-        if(baseRep.every(number => number === '1')) {
-            // console.log(currentBase)
-        }else {
-            base = baseNumbers[1] + 1
-            number = s
-        }
-        // console.log(baseRep)
-    }
+   }
 }
 
-lowestBase(1000002)
+lowestBase(1001001)
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let number = s
+// let base = 2
+// console.log(s.toString(20))
+// while(number !== 0) {
+//     let baseNumbers = [1]
+//     let currentBase = base
+//     let baseRep = []
+//     while(base < number) {
+//         baseNumbers.push(base)
+//         base *= currentBase
+//     }
+//     console.log(baseNumbers)
+//     for(let i = baseNumbers.length - 1;i >= 0;i--) {
+        
+//         if(number - baseNumbers[i] >= 0) {
+//             number -= baseNumbers[i]
+//             baseRep.push('1')
+//         }else {
+//             baseRep.push('0')
+//         }
+//         if(i === 0 && number !== 0) {
+//             baseRep.push('0')
+//         }
+//     }
+//     if(baseRep.every(number => number === '1')) {
+//         console.log(currentBase)
+//     }else {
+//         base = baseNumbers[1] + 1
+//         number = s
+//     }
+//     console.log(baseRep)
+// }
 
 
 
