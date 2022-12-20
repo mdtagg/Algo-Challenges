f9917f8d0c3471aeb798321d8f58a70f64dcdfda
Mike Tagg <mdtagg91@gmail.com>
2022-12-20 13:07:28 -0800

    solution for lowest base system that is efficient enough to not crash the browser has not been found
    getting a binary representation of the given number and finding the base from there is thread I stopped on

--- Hard/lowest_base_system.js
+++ Hard/lowest_base_system.js
@@ -1,33 +1,250 @@
 
 function lowestBase(s) {
-    let numbers = []
-    for(let i = 2;i < 11;i++) {
-        if((s-1) % i === 0) {
-            console.log(i)
-            numbers.push(i)
-        }else if(!numbers.length) {
-            console.log(s - 1)
-            break
+    let test = 16
+    console.log(test.toString(4))
+    // console.log(test)
+    // console.log(s.toString(2))
+    let sArray = s.toString(2).split('')
+    console.log(sArray)
+    let baseArray = [1]
+    let base = 2
+    for(let i = sArray.length - 2;i >= 0;i--) {
+        if(sArray[i] === '1') {
+            baseArray.push(base)
+        }else {
+            baseArray.push(0)
         }
-        
-   }
-   for(let i = numbers.length - 1;i >= 0;i--) {
-        let base = numbers[i]
-        let prevBase = numbers[i]
-        while(base <= s) {
-            base *= base
-            base += prevBase
-            prevBase = base
-            // console.log(base)
-            if(base === s - 1) {
-                console.log(numbers[i])
-                break
-            }
-        }
-   }
+        base*=2
+    }
+    console.log(baseArray.reverse())
+    // console.log(21 + 2 - 3 + 8 - 16)
 }
+    lowestBase(21)
+    // 125002500050001
+    
+//     let numbers = [2,3,4,5,6,7,8,9,10]
+//     let newS = s - 1 
+//     let test = false
+//     while(test === false) {  
+//         for(let i = 0;i < numbers.length;i++) {
+//             let base = numbers[i]
+//             let testArr = [1]
+//             while(base <= newS) {
+//                 let reduced = testArr.reduce((prev,curr) => prev + curr,base)
+//                 if(newS % reduced !== 0) {
+//                     break
+//                 }
+//                 testArr.push(base)
+//                 base *= numbers[i]
+//             }
+//             let testFunc = testArr.reduce((prev,curr) => prev + curr)
 
-lowestBase(1001001)
+//             if(testFunc === s) {
+//                 console.log(testArr)
+//                 console.log(numbers[i])
+//                 test = true
+//                 break
+//             }
+//             else if(i === 8) {
+//                 numbers = numbers.map(number => number * 10)
+//                 // console.log('numbers: ',numbers)
+//                 test = true
+//             }
+//             // console.log(testArr)
+//         }
+//         // break
+//     }
+// }
+
+
+// console.log(numbers)
+// if(!numbers.length) {
+//     console.log(newS)
+// }else {
+// // console.log(numbers)
+// numbers = numbers.filter(number => number**2 < newS)
+// console.log(numbers)
+// // console.log(s % (10 * 10 + 1 + 10))
+// numbers = numbers.filter(number => newS % (number * number) <= 1)
+// console.log(numbers)
+// console.log(numbers[numbers.length - 1])
+// if(!numbers.length) {
+//     console.log(s - 1)
+// }
+// }
+
+
+
+
+// console.log(s.toString(2))
+//     let numbers = []
+//     let newS = s - 1
+//     let newTest = []
+//     console.log(s.toString(2))
+
+//     if(newS % 10 === 0) {
+//         let test = newS.toString().split('')
+//         for(let i = test.length - 1;i > 0;i--) {
+//             if(test[i] !== '0') {
+//                 newTest = test.splice(i)
+//                 break
+//             }
+//         }
+//         console.log(parseInt(newTest.join('')))
+//         return parseInt(newTest.join(''))
+//     }else {
+
+//     for(let i = 2;i < 10;i++) {
+//         if((s-1) % i === 0) {
+//             numbers.push(i)
+//         }else if(!numbers.length) {
+//             break
+//         }
+//     }
+//     console.log(numbers)
+
+//     for(let i = numbers.length - 1;i >= 0;i--) {
+//         let base = numbers[i]
+//         let prevBase = numbers[i]
+//         while(base <= s) {
+//             base *= base
+//             base += prevBase
+//             prevBase = base
+//             // console.log(base)
+//             if(base === s - 1) {
+//                 console.log(numbers[i])
+//                 return numbers[i]
+//             }else if(i === 0) {
+//                 // numbers = numbers.map(number => number * 10)
+//                 // console.log(numbers)
+//                 break
+//             }
+//         }
+//     }
+// }
+
+
+// let newS = s - 1
+//     let newArray = s.toString().split('')
+//     let numbers = []
+//     console.log(s.toString(2))
+
+//     for(let i = 2;i < 11;i++) {
+//         if(newS % i === 0) {
+//         numbers.push(i)
+//     }
+    
+//    }
+//    console.log(numbers)
+   
+//    for(let i = 0;i < numbers.length;i++) {
+
+//     let base = numbers[i]
+//     let prevBase = numbers[i]
+
+//     while(base < newS) {
+//         console.log(base)
+//         base *= base
+//         base += prevBase
+//         prevBase = base
+        
+//         }if(newS % base !== 0) {
+
+//             break
+//         }
+//         if(base === newS) {
+//             console.log('success: ',numbers[i])
+//            break
+//         } 
+//     }
+
+
+// let test = s.toString(2).split('')
+//     console.log(test)
+//     let newTest = 1
+//     console.log(parseInt('111',2))
+//     let testFunc = test.every(item => item === '1')
+//     console.log(testFunc)
+//     if(testFunc) {
+//         console.log(2)
+//     }else {
+//     while(!test.every(item => item === '1')) {
+//     for(let i = 0;i < test.length;i++) {
+//         if(test[0] === '0') {
+//             test.splice(0,1)
+//         }
+//         else if(test[i + 1] === '0') {
+//             test[i] = '0'
+//             test[i + 1] = '1'
+//             newTest++
+//             console.log(test)
+//         }
+//     }
+// }
+// console.log(newTest)
+//     }
+
+
+// 125002500050001
+
+
+// let baseNumbers = []
+//     let numbers = []
+//     let newS = s - 1
+//     console.log(s.toString(2))
+//     console.log()
+//     for(let i = 2;i < 11;i++) {
+//         if(newS % i === 0) {
+//             numbers.push(i)
+//         }
+//     }
+//     if(!numbers.length) {
+//         console.log(newS)
+//     }else {
+//     console.log(numbers)
+//     while(s > 0) {
+//         let sqrt = Math.floor(Math.sqrt(s))
+//         if(s === 1) {
+//             console.log(baseNumbers[baseNumbers.length - 1])
+//             break
+//         }
+//         s -= sqrt**2 
+//         baseNumbers.push(s)
+//     }
+//     console.log(baseNumbers)
+// }
+
+
+
+
+
+
+
+
+
+// console.log(s)
+//         let newTest = newS.toString().split('')
+//         let number = ""
+//         for(let i = newTest.length;i > 0;i--) {
+//             console.log(newTest[i])
+//             if(newTest[i] !== 0) {
+//                 number = newTest[i]
+//                 break
+//             }
+//         }
+//         console.log(number)
+//         let index = test.lastIndexOf('1')
+//         test.splice(0,index)
+//         test = parseInt(test.join(''))
+
+
+
+
+
+
+
+
+
 
 
 
