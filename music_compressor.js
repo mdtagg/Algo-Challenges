8070234c8d6b79a6b1f822be59560121f9a72e37
Mike Tagg <mdtagg91@gmail.com>
2022-12-23 15:57:31 -0800

    solutions for all conditions found, proceeding to test

--- music_compressor.js
+++ music_compressor.js
@@ -1,43 +1,43 @@
 function compress(music) {
     
-    let consecutive = []
-    let interval = []
     let compressedArray = []
 
     while (music.length) {
-        let current = music[0]
-        if(music[0] === music[1]) {
-            let identical = []
-            let consecutiveCount = 0
+
+        let intervalTest = Math.round(Math.sqrt((music[1] - music[0])**2))
+        let intervalTestTwo = Math.round(Math.sqrt((music[2] - music[1])**2))
+
+        if(intervalTest === 0) {
+            let current = music[0]
+            let identicalCount = 0
             for(let i = 0;i < music.length;i++) {
-                if(music[i] === current) {
-                    consecutiveCount++
+                if(music[i] === music[0]) {
+                    identicalCount++
                 }else {
                     break
                 }
             }
-            identical.push(...music.splice(0,consecutiveCount))
-            compressedArray.push(`${current}*${identical.length}`)
+            music.splice(0,identicalCount)
+            compressedArray.push(`${current}*${identicalCount}`)
         }
-        
-        else if(current === next - 1 && next === music[2] - 1) {
-            while(music[0] === music[1] - 1) {
+
+        else if(intervalTest === 1 && intervalTestTwo === 1) {
+            let consecutive = []
+         
+            while(Math.round(Math.sqrt((music[1] - music[0])**2)) === 1) {
                 consecutive.push(...music.splice(0,1))
             }
             consecutive.push(...music.splice(0,1))
             compressedArray.push(`${consecutive[0]}-${consecutive[consecutive.length - 1]}`)
-            consecutive = []
         }
 
-        else if(Math.round(Math.sqrt((music[1] - music[0]))**2) === 
-        Math.round(Math.sqrt((music[2] - music[1])**2))) {
-            let intervalNum = music[1] - music[0]
-            while(music[1] - music[0] === intervalNum) {
+        else if(intervalTest === intervalTestTwo) {
+            let interval = []
+            while(Math.round(Math.sqrt((music[1] - music[0])**2)) === intervalTest) {
                 interval.push(...music.splice(0,1))
             }
             interval.push(...music.splice(0,1))
-            compressedArray.push(`${interval[0]}-${interval[interval.length - 1]}/${intervalNum}`)
-            interval = []
+            compressedArray.push(`${interval[0]}-${interval[interval.length - 1]}/${intervalTest}`)
         }
         else {
             compressedArray.push(music.splice(0,1))
@@ -47,7 +47,7 @@ function compress(music) {
     console.log(compressedArray)
 
 }
-compress([1,1,1,5,5,5])
+compress([1,10,8,6,7])
 
 
 // let single = []
