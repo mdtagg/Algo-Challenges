dee3d33f83756613f2f8f98622eef25cb1fec8d4
Mike Tagg <mdtagg91@gmail.com>
2022-12-23 14:15:31 -0800

    moved to medium folder

--- Medium/human_readable_time.js
+++ Medium/human_readable_time.js
@@ -21,9 +21,26 @@ function humanReadable (seconds) {
 
 humanReadable(90)
 
-// for(let i = 0;i < time.length;i++) {
-    //     let newNumber = '0' + time[i].toString()
-    //     if(time[i].toString().length === 1) {
-    //         time.splice(i,1,newNumber)
-    //     }
-    // }
+// function humanReadable (seconds) {
+//     let hours = 0
+//     let minutes = 0
+//     let time = []
+//     while(seconds / 3600 >= 1) {
+//         seconds -= 3600
+//         hours++
+//     }
+//     time.push(hours)
+//     while(seconds / 60 >= 1) {
+//         seconds -=60
+//         minutes++
+//     }
+//     time.push(minutes)
+//     time.push(seconds)
+//     for(let i = 0;i < time.length;i++) {
+//         let newNumber = '0' + time[i].toString()
+//         if(time[i].toString().length === 1) {
+//             time.splice(i,1,newNumber)
+//         }
+//     }
+//     return time.join(':')
+// }
