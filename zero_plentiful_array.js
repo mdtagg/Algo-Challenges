eb818cc41d554915d87c17c504f7566a6b06c49c
Mike Tagg <mdtagg91@gmail.com>
2022-12-31 19:08:19 -0800

    zeroPlentiful challenge completed

---
+++ zero_plentiful_array.js
@@ -0,0 +1,77 @@
+function zeroPlentiful(arr){
+    let zeroArr = []
+    while(arr.length) {
+        let zero = []
+        if(arr[0] === 0) {
+            while(arr[0] === 0) {
+                zero.push(...arr.splice(0,1))
+            }
+            zeroArr.push(zero)
+        }else if(arr[0] !== 0) {
+            arr.splice(0,1)
+        }
+    }
+    console.log(zeroArr.every(item => item.length >= 4) ? zeroArr.length : 0)
+}
+
+
+zeroPlentiful([1000,0,1,0,0,0,0])
+
+
+// let test = []
+//     for(let i = 0;i < arr.length;i++) {
+//         if(arr[i] === 0) {
+//             let zeros = []
+//             while(arr[i] === 0) {
+//                 zeros.push(arr.splice(0,1))
+//             }
+//             console.log(zeros)
+//         }
+//         test.push(arr.splice(0,1))
+        
+//     }
+//     console.log(test)
+
+// let totalZeros = 0
+//     let zeroCount = 0
+//     for(let i = 0;i < arr.length;i++) {
+//         if(arr[i] === 0) {
+//             zeroCount++
+//         } else if(arr [i] !== 0) {
+//             totalZeros = 0
+//             break
+//         }
+//     }
+
+// let regex = /0{1,}/g
+//     let zeroTest = arr.join('').match(regex)
+//     console.log(zeroTest)
+//     if(zeroTest.every(item => item.length >= 4)) {
+//         console.log(zeroTest.length)
+//     }else {
+//         console.log(0)
+//     }
+
+
+
+
+// for(let i = 0;i < arr.length;i++) {
+//     if(arr[i] === 0) {
+//         zeroCount++
+//     } else if(arr[i] !== 0 && zeroCount !== 0) {
+//         totalZeros = 0
+//         break
+//     }
+//     else {
+//         zeroCount = 0
+//     }
+//     if(zeroCount >= 4 && arr[i + 1] !== 0) {
+//         totalZeros++
+//         zeroCount = 0
+//     }
+//     // console.log(zeroCount)
+// }
+// console.log(totalZeros)
+
+
+
