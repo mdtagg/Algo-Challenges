709c7e406fa82d8f4630753d1be9d4142cc3eb11
Mike Tagg <mdtagg91@gmail.com>
2022-12-31 19:34:43 -0800

    best solution added for plentiful zeros challenge

--- zero_plentiful_array.js
+++ zero_plentiful_array.js
@@ -2,20 +2,59 @@ function zeroPlentiful(arr){
     let zeroArr = []
     while(arr.length) {
         let zero = []
-        if(arr[0] === 0) {
-            while(arr[0] === 0) {
-                zero.push(...arr.splice(0,1))
-            }
-            zeroArr.push(zero)
-        }else if(arr[0] !== 0) {
-            arr.splice(0,1)
+        while(arr[0] === 0) {
+            zero.push(...arr.splice(0,1))
         }
+        zero.length ? zeroArr.push(zero) : arr.splice(0,1)
     }
     console.log(zeroArr.every(item => item.length >= 4) ? zeroArr.length : 0)
 }
 
+//BEST SOLUTION (most clever in this case)
+
+function zeroPlentiful(arr){
+    var sequences = arr.map((x) => !x ? x : ',').join('').split(',').filter((str) => str);
+    console.log(arr.map((x) => !x ? x : ',').join('').split(','))
+    console.log(sequences.every((str) => str.length >= 4) ? sequences.length : 0);
+  }
+
+/*
+
+- zero is falsy so if you need to filter numbers apart from zero you can use
+it as a boolean. 
+
+-.filter can be used to remove blank spaces. 
+
+The arr.map is checking to see if each number is true or false, which in the 
+case of 0 is true because zero is a falsy value. Filter is used to remove 
+any blank space in an array. 
+*/
 
 zeroPlentiful([1000,0,1,0,0,0,0])
+
+
+
+
+
+
+
+
+
+// function zeroPlentiful(arr){
+//     let zeroArr = []
+//     while(arr.length) {
+//         let zero = []
+//         if(arr[0] === 0) {
+//             while(arr[0] === 0) {
+//                 zero.push(...arr.splice(0,1))
+//             }
+//             zeroArr.push(zero)
+//         }else if(arr[0] !== 0) {
+//             arr.splice(0,1)
+//         }
+//     }
+//     return zeroArr.every(item => item.length >= 4) ? zeroArr.length : 0
+// }
 
 
 // let test = []
