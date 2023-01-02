c73ce7db1ddd12fcd7df63eb6a01c34972b894a4
Mike Tagg <mdtagg91@gmail.com>
2023-01-01 19:00:31 -0800

    final solution for bankers plan added

--- 6th_kyu/fortune_calc.js
+++ 6th_kyu/fortune_calc.js
@@ -1,16 +1,13 @@
 function fortune(f0, p, c0, n, i) {
-    let total = f0
-    let livingAmt = c0
     let interest = p * .01
     let inflation = i * .01
     while(n > 1) {
-        total = Math.floor(total + (interest * total) - c0)
+        f0 = Math.floor(f0 + (interest * f0) - c0)
         c0 += Math.floor((c0 * inflation))
-        console.log(total)
+        console.log(f0)
         n--
     }
-    console.log(total < 0 ? false : true)
-    // console.log(true)
+    console.log(f0 < 0 ? false : true)
 }
 
 fortune(10000, 0, 10000, 2, 0)
