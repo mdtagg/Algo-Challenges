afa19b7e58ef53e87cf6c84f5fb8f41e678374d8
Mike Tagg <mdtagg91@gmail.com>
2023-01-08 12:29:31 -0800

    maze path finder challenge added

--- 4th_kyu/strip_comment.js
+++ 4th_kyu/strip_comment.js
@@ -16,3 +16,21 @@ function solution(input, markers) {
   };
 
 solution("apples, plums % and bananas\npears\noranges !applesauce",['%','!'])
+
+
+//BEST SOLUTION
+
+function solution(input, markers) {
+    return input.split('\n').map(
+      line => markers.reduce(
+        (line, marker) => line.split(marker)[0].trim(), line
+      )
+    ).join('\n')
+  }
+
+  /*
+  This solution has a clever use of the reduce method but the only thing is 
+  that the description of the challenge does not say that the markers argument
+  has to be an array. So if there is only one string value as an input for 
+  markers this solution breaks. 
+  */
