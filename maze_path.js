2eb679cddf8a78ba7506e7dd2f5d9aefd7767186
Mike Tagg <mdtagg91@gmail.com>
2023-01-08 19:22:17 -0800

    working toward maze solution

--- maze_path.js
+++ maze_path.js
@@ -1,42 +1,96 @@
 function pathFinder(maze) {
     const layers = maze.split('\n').map(item => item.trim())
     let currentPosition = [0,0]
-    let nextMove = true
-    let lastMove = null
-    let moves = 0
+    let lastMove = ''
+    let count = 0
 
-    while(nextMove) {
-        let positions = {
-            left:[currentPosition[0],currentPosition[0]-1],
-            up:[currentPosition[0] - 1,currentPosition[1]],
-            right: [currentPosition[0],currentPosition[0] + 1],
-            down: [currentPosition[0] + 1,currentPosition[1]]
+    while(count < 4) {
+        let row = currentPosition[0]
+        let column = currentPosition[1]
+        // console.log(row,column)
+        let possibleMoves = []
+        if(row - 1 !== -1 && lastMove !== 'down') {
+            possibleMoves.push('up')
+        }if (column !== layers[0].length - 1 && lastMove !== 'left') {
+            possibleMoves.push('right')
+        }if(row + 1 !== layers[0].length && lastMove !== 'up') {
+            possibleMoves.push('down')
+        }if(column - 1 !== -1 && lastMove !== 'right') {
+            possibleMoves.push('left')
         }
-        let possibleMoves = [positions.left,positions.right,positions.down,positions.up]
-        .filter(move => {
-            return move.includes(-1) || move.includes(3) ? false : true
-        })
-        .filter(move => {
-            return layers[move[0]][move[1]] === 'W' ||
-            move == lastMove ? false : true
-        })
         console.log(possibleMoves)
-        currentPosition = possibleMoves
-        lastMove = currentPosition
-        moves++
-        if(!currentPosition.length) break
-        if(moves > 5) break
-        console.log(moves)
-       
-    }
-    
+        possibleMoves = possibleMoves.map(move => {
+            switch(move) {
+                case 'right':
+                    return ['right',[row,column + 1]]
+                case 'down':
+                    return ['down',[row + 1,column]]
+                case 'up':
+                    return ['up',[row - 1,column]]
+                case 'left':
+                    return ['left',[row,column - 1]]
+            }
+        })
+        // console.log(possibleMoves)
+        .filter(move => {
+            console.log(move[1][0],move[1][1])
+            return layers[move[1][0]][move[1][1]] === 'W' ? false : true
+        })
+        if(!possibleMoves.length) {
+            console.log(false)
+            return false
+        }
+        if(possibleMoves > 1) {
 
+        }
+        console.log(possibleMoves)
+        lastMove = possibleMoves[0][0]
+        console.log(lastMove)
+        count++
+        currentPosition = possibleMoves[0][1]
+        console.log(currentPosition)
+
+        // break
+    }
 }
 
 
+
   pathFinder(`.W.
-              .W.
-              W..`)
+  .W.
+  W..`)
+
+//               const layers = maze.split('\n').map(item => item.trim())
+//               let currentPosition = [0,0]
+//               let nextMove = true
+//               let lastMove = null
+//               let moves = 0
+          
+//               while(nextMove) {
+//                   let positions = {
+//                       left:[currentPosition[0],currentPosition[0]-1],
+//                       up:[currentPosition[0] - 1,currentPosition[1]],
+//                       right: [currentPosition[0],currentPosition[0] + 1],
+//                       down: [currentPosition[0] + 1,currentPosition[1]]
+//                   }
+//                   let possibleMoves = [positions.left,positions.right,positions.down,positions.up]
+//                   .filter(move => {
+//                       return move.includes(-1) || move.includes(3) ? false : true
+//                   })
+//                   .filter(move => {
+//                       return layers[move[0]][move[1]] === 'W' ||
+//                       move.join('') == lastMove.join('') ? false : true
+//                   })
+//                   console.log(possibleMoves)
+//                   currentPosition = possibleMoves
+//                   lastMove = currentPosition
+//                   console.log(lastMove)
+//                   moves++
+//                   if(!currentPosition.length) break
+//                   if(moves > 5) break
+//                   console.log(moves)
+                 
+//               }
 
 
 // if(layers[0][1] === 'W') {
