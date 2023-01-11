921764c8db30c559ed2023e6e4f3f8cf8b123da4
Mike Tagg <mdtagg91@gmail.com>
2023-01-10 17:25:56 -0800

    Added logic for number of nines challenge
    while loop added that removes all numbers in the ones place so that n is divisible by nine
    added logic that turns n into an array of numbers and then reverses it so that a for loop can
    iterate through each number place.

--- count_nines.js
+++ count_nines.js
@@ -1,27 +1,52 @@
 function number9(n){
-    // let multiplier = 1
-    // n = n.toString().split('').reverse().map(item => parseInt(item))
-    // let test = n.map(item => {
-        
-    //     multiplier *= 10
-    // });
-    // console.log(n);
-    let nines = []
-    for(let i = 1; i <= n; i++){
-        nines.push(i)
+    while(n.toString().split('').pop() !== '9') {
+        n--
     }
-    // console.log(nines)
-    // let numNines = 0
-    // for(let i = 0;i < nines.length;i++){
-    //     if(nines[i].toString().split('').includes('9')) {
-    //         numNines += 1
-    //     }
-    // }
-    console.log(nines)
-    // console.log(numNines)
+    console.log(n)
+    let multiplier = 1
+    n = n.toString().split('').map(item => parseInt(item)).reverse()
+    console.log(n)
+    let numNines = 0
+    let lastNumber = 0
+    let test = n.map(number => {
+        let item = number * multiplier
+        console.log(item)
+        if(item === 9) {
+            numNines += 1
+        }else {
+            numNines += item / multiplier
+        }
+        console.log(item,multiplier)
+        multiplier *= 10
+        
+    })
+    console.log(numNines)
 }
 
-number9(10000000000)
+number9(100)
+
+//10 nines for 90, 9 nines for 90 thru 9, 1 nine for 9 in the ones 
+
+// // let multiplier = 1
+//     // n = n.toString().split('').reverse().map(item => parseInt(item))
+//     // let test = n.map(item => {
+        
+//     //     multiplier *= 10
+//     // });
+//     // console.log(n);
+//     let nines = []
+//     for(let i = 1; i <= n; i++){
+//         nines.push(i)
+//     }
+//     // console.log(nines)
+//     // let numNines = 0
+//     // for(let i = 0;i < nines.length;i++){
+//     //     if(nines[i].toString().split('').includes('9')) {
+//     //         numNines += 1
+//     //     }
+//     // }
+//     console.log(nines)
+//     // console.log(numNines)
 
 // 9 = one nine
 // 9 - 90 = 
