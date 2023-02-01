1b8d46132c7a9fafc7f0a8c15bb7e2f0aa9ba21b
Mike Tagg <mdtagg91@gmail.com>
2023-02-01 10:16:24 -0800

    remove value leet code challenge added

---
+++ leet_code/easy/remove_element.js
@@ -0,0 +1,26 @@
+var removeElement = function(nums, val) {
+    let k = 0
+    for(let i = 0;i < nums.length;i++) {
+        nums[i] === val ? nums[i] = '_' : k++
+    }
+    nums.sort()
+    return k
+};
+
+
+//BEST SOLUTION
+
+var removeElement = function (nums, val) {
+    let po = 0;
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] != val) {
+            nums[po] = nums[i];
+            po++;
+        }
+    }
+    console.log(nums)
+    return po;
+};
+
+
+removeElement([3,2,2,3],3)
