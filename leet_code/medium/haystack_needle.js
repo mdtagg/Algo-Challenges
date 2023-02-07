4d60b3d1e6308af9a18fbf7b8a409cba4a1151d2
Mike Tagg <mdtagg91@gmail.com>
2023-02-07 15:16:02 -0800

    haystack needle challenge added

---
+++ leet_code/medium/haystack_needle.js
@@ -0,0 +1,9 @@
+/*
+Given two strings needle and haystack, return the index of the first occurrence of 
+needle in haystack, or -1 if needle is not part of haystack.
+*/
+
+
+var strStr = function(haystack, needle) {
+    return haystack.search(needle)
+};
