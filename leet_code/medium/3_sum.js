e892825c3d1c88c92f7555ec07250ff9658b6fe9
Mike Tagg <mdtagg91@gmail.com>
2023-02-07 14:34:52 -0800

    solution added for 3 sum challenge, added to leet code medium

--- leet_code/medium/3_sum.js
+++ leet_code/medium/3_sum.js
@@ -1,37 +1,37 @@
+/*
+Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that 
+i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+
+Notice that the solution set must not contain duplicate triplets.
+*/
+
+
 var threeSum = function(nums) {
-    
+    nums = nums.sort((a,b) => a - b)
+    // console.log(nums)
+    let validSums = []
+    let validSumStrs = []
+    for(let i = 0;i < nums.length;i++) {
+        let left = i + 1
+        let right = nums.length - 1
+        while(left < right) {
+            let potentialSum = [nums[i],nums[left],nums[right]]
+            // console.log(potentialSum)
+            let sum = nums[i] + nums[left] + nums[right]
+            if(sum > 0) {
+                right--
+            }else if(sum === 0 && !validSumStrs.includes(potentialSum.join(''))) {
+                validSums.push(potentialSum)
+                validSumStrs.push(potentialSum.join(''))
+                left++
+            }else {
+                left++
+            }
+        }
+        if(i === nums.length - 3) break
+    }
+    console.log(validSums)
 }
-
-threeSum([-1,0,1,2,-1,-4])
-
-var threeSum = function(nums) {
-	nums.sort((a,b) => a-b)
-	const result=[]
-
-	if(nums.length < 3) return result
-
-	for(let i=0; i< nums.length; i++){
-		let left = i+1
-		let right = nums.length-1
-		if(i > 0 && nums[i] === nums[i-1]) continue
-		while(left < right){
-			let sum = nums[i] + nums[left] + nums[right]
-            console.log({sum},nums[i],nums[left],nums[right])
-			if(sum===0){
-				let arr=[nums[i], nums[left], nums[right]]
-				result.push(arr)
-				left++
-				while(nums[left] === nums[left-1] && left < right) left++
-			}else if(sum > 0){
-				right--
-			}else{
-				left++
-			}
-		}
-	}
-    console.log(nums)
-	// console.log(result)
-};
 
 threeSum([-1,0,1,2,-1,-4])
 
