c65a3c17d78efd29173e37a800f252e66c21e4cc
Mike Tagg <mdtagg91@gmail.com>
2023-02-06 18:52:03 -0800

    description issue added to most water

--- not_completed/leetCode/most_water.js
+++ not_completed/leetCode/most_water.js
@@ -1,26 +1,33 @@
+//this challenge is incorrectly described. Water volume has nothing to do with the challenge, 
+// [1,2,1] expects an output of 2, so for this to work there has a to be a hole in the middle wall
+//to allow for water to fill the 1 chambers. 
+
+
 var maxArea = function(height) {
     
-    let containerLeft = height.indexOf(Math.max(...height))
-    let newContainer = Array.from(height)
-    let maxVolume = 0
-    let count = 0
-    while(newContainer[newContainer.length - 1] !== 0) {
-        let containerRight = height.lastIndexOf(Math.max(...newContainer))
-        let containerWidth = containerRight - containerLeft
-        let containerHeight = Math.min(...[height[containerLeft],height[containerRight]])
-        let containerVolume = containerWidth * containerHeight
-        if(maxVolume < containerVolume) {
-            maxVolume = containerVolume
-        }
-        newContainer[containerLeft] = 0
-        newContainer[containerRight] = 0
-        count++
-        // break
-    }
-    console.log(maxVolume)
+    
 };
 
-maxArea([1,8,6,2,5,4,8,3,7,6])
+maxArea([1,2,1])
+
+
+// [1,8,6,2,5,4,8,3,7]
+
+// let containerLeft = height.indexOf(Math.max(...height))
+//     let newContainer = Array.from(height)
+//     let maxVolume = 0
+//     while(newContainer[newContainer.length - 1] !== 0) {
+//         let containerRight = height.lastIndexOf(Math.max(...newContainer))
+//         let containerWidth = containerRight - containerLeft
+//         let containerHeight = Math.min(...[height[containerLeft],height[containerRight]])
+//         let containerVolume = containerWidth * containerHeight
+//         if(maxVolume < containerVolume) {
+//             maxVolume = containerVolume
+//         }
+//         newContainer[containerLeft] = 0
+//         newContainer[containerRight] = 0
+//     }
+//     console.log(maxVolume)
 
 // let maxVolume = 0
 //     let highestStart = height.indexOf(Math.max(...height))
