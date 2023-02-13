3a6960772c3a321afe01be06ff70e051537925d2
Mike Tagg <mdtagg91@gmail.com>
2023-02-13 11:13:00 -0800

    moved max water distance into not completed

---
+++ not_completed/leetCode/max_water_distance.js
@@ -0,0 +1,41 @@
+var maxDistance = function(grid) {
+    
+
+};
+
+
+maxDistance([[1,0,1],[0,0,0],[1,0,1]])
+
+
+// let gridMap = Array.from(grid)
+//     gridMap = gridMap.map(row => {
+//         return row.map(item => {
+//             if(item === 0) {
+//                 return 1
+//             } else {
+//                 return -1
+//             }
+//         })
+//     })
+//     console.log(gridMap)
+
+// let maxDistance = 1
+//     for(let i = 0;i < grid.length;i++) {
+//         for(let j = 0;j < grid[i].length;j++) {
+            
+//         }
+//     }
+
+
+
+// let maxDistance = 0
+//     grid.forEach((item,index) => {
+//         maxDistance = index
+//         item.forEach(number => {
+//             if(number === 1) {
+//                 maxDistance--
+//             }else {
+//                 maxDistance++
+//             }
+//         })
+//     })
