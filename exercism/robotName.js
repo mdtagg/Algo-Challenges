4994c56f75b9e9a043dc549f0d0b0233fce65248
Mike Tagg <mdtagg91@gmail.com>
2023-04-12 14:00:48 -0700

    robot name challenge one test away from passing solution

--- exercism/robotName.js
+++ exercism/robotName.js
@@ -1,11 +1,23 @@
 class Robot {
+
+    static robotNames
+
     constructor() {
-      this.robotNames = new Map()
-      this.mapIndex = 1
+      Robot.robotNames = new Map()
+      this.robotName = this.generateName(0)
+      this.mapIndex = 0
     }
   
     get name() {
-        const roboName = 
+        return this.robotName
+    }
+
+    generateName(mapIndex) {
+      
+      if(mapIndex !== 0) {
+        mapIndex = this.mapIndex
+      }
+      const roboName = 
         [
             getRandomLetter(),
             getRandomLetter(),
@@ -15,38 +27,54 @@ class Robot {
         ]
         .join('')
 
-        this.robotNames.set(this.mapIndex,roboName)
+        Robot.robotNames.set(mapIndex,roboName)
         this.mapIndex += 1
 
         return roboName
     }
   
     resetName() {
-        this.robotNames.delete(this.mapIndex)
-        this.mapIndex -= 1
-        return this.name
+        const newName = this.generateName()
+        const listOfNames = Array.from(Robot.robotNames.values())
+        console.log(newName,listOfNames)
+        // if(listOfNames.includes(newName)) {
+        //   this.resetName()
+        //   return
+        // }
+        // this.robotName = this.generateName()
+        // return this.name
     }
   
-    releaseNames() {
-        console.log(this.robotNames)
-      return this.robotNames
+    static releaseNames() {
+      return this.robotNames.clear()
     }
   }
 
   function getRandomLetter() {
     const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-    const randomIndex = Math.ceil(Math.random() * 26)
+    const randomIndex = Math.floor(Math.random() * 26)
     const nameLetter = alphabet[randomIndex]
     return nameLetter
   }
 
   function getRandomNumber() {
-    const randomNumber = (Math.ceil(Math.random() * 10)).toString()
+    const randomNumber = (Math.floor(Math.random() * 10)).toString()
     return randomNumber
   }
   
+
 const robot = new Robot()
-robot.name
-robot.name
+console.log(robot.name)
+robot.generateName()
+// robot.generateName()
+// robot.generateName()
 robot.resetName()
-robot.releaseNames()
+
+console.log(Robot.robotNames)
+// console.log(Array.from(Robot.robotNames.values()))
+// console.log(Robot.robotNames)
+// console.log(Array.from(Robot.robotNames.values()))
+
+// robot.name
+// robot.resetName()
+// robot.releaseNames()
