0e2cb8113f92e8c5013b079e1ba58b03181d717e
Mike Tagg <mdtagg91@gmail.com>
2023-04-13 13:48:04 -0700

    clock challenge updated

--- exercism/clock.js
+++ exercism/clock.js
@@ -1,62 +1,84 @@
 class Clock {
     constructor(hours, minutes) {
-      this.parsedTime = this.parseTime(hours,minutes) 
+        this.time = this.parseTime(hours,minutes)
     }
 
-    parseTime(hours,minutes,operation) {
-        let hoursToAdjust = Math.floor(minutes / 60)
-        // if(operation === 'plus') {
-        //     hoursToAdjust = Math.floor(minutes / 60)
-        // }
-        let adjustedHours = (hours + hoursToAdjust) % 24
-        let adjustedMinutes = minutes % 60
+    parseTime(hours,minutes) {
+        const { newMinutes, hoursToAdjust } = this.parseMinutes(minutes)
+        const newHours = this.parseHours(hours,hoursToAdjust)
+        return { newMinutes, newHours }
+    }
 
+    parseMinutes(minutes) {
+        let hoursToAdjust = Math.floor(minutes / 60)
+        let adjustedMinutes = minutes % 60
         const newMinutes = this.adjustTime(adjustedMinutes,60)
+        return { newMinutes,hoursToAdjust }
+    }
+
+    parseHours(hours,hoursToAdjust) {
+        let adjustedHours = (hours + hoursToAdjust) % 24
         const newHours = this.adjustTime(adjustedHours,24)
-        
-        return { newHours, newMinutes }
+
+        return newHours
     }
 
     adjustTime(timeToAdjust,adjustment) {
-        const adjustedArray = timeToAdjust.toString().split('')
 
+        let adjustedArray = timeToAdjust.toString().split('')
         if(timeToAdjust < 0) {
             timeToAdjust += adjustment
         }
         if(adjustedArray.length === 1) {
             adjustedArray.unshift('0')
-            timeToAdjust = adjustedArray.join('')
+            timeToAdjust = parseInt(adjustedArray.join(''))
+            // adjustedArray = adjustedArray.join('')
         }
         return timeToAdjust
     }
   
     toString() {
-        const { newHours, newMinutes } = this.parsedTime
+        const { newHours,newMinutes } = this.time
         return `${newHours}:${newMinutes}`
     }
   
     plus(minutes) {
-        const { newHours, newMinutes } = this.parsedTime
-        const adjustedMinutes = newMinutes + minutes
-        this.parsedTime = this.parseTime(newHours,adjustedMinutes)
+        const { newHours, newMinutes } = this.time
+        const minutesToAdd = newMinutes + minutes
+        return this.time = this.parseTime(newHours,minutesToAdd)
     }
   
     minus(minutes) {
-        const { newHours, newMinutes } = this.parsedTime
-        const adjustedMinutes = newMinutes - minutes
-        this.parsedTime = this.parseTime(newHours,adjustedMinutes)
+        const { newHours, newMinutes } = this.time
+        const adjustedMinutes = parseInt(newMinutes) + minutes * -1
+        const adjustedHours = parseInt(newHours)
+        this.time = this.parseTime(adjustedHours,adjustedMinutes)
     }
   
     equals(other) {
-      throw new Error('Remove this statement and implement this function')
+        other === this.time
     }
   }
   
-  const clock = new Clock(72,8640)
+  const clock = new Clock(8)
 //   console.log(clock.toString())
   clock.plus(50)
   clock.minus(51)
   console.log(clock.toString())
+
+
+//   parseTime(hours,minutes) {
+    //     // let hoursToAdjust = Math.floor(minutes / 60)
+    //     // let adjustedHours = (hours + hoursToAdjust) % 24
+    //     // let adjustedMinutes = minutes % 60
+
+    //     // const newMinutes = this.adjustTime(adjustedMinutes,60)
+    //     // const newHours = this.adjustTime(adjustedHours,24)
+        
+    //     return { newHours, newMinutes }
+    // }
+
+
 
   // if(adjustedMinutes < 0) {
         //     adjustedMinutes += 60
