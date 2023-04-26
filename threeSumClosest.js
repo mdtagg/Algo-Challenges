c01261fae27e71ba9f1c14ebe721a82a5b4cd21a
Mike Tagg <mdtagg91@gmail.com>
2023-04-26 13:20:53 -0700

    3 sum closest challenge updated

--- threeSumClosest.js
+++ threeSumClosest.js
@@ -1,34 +1,42 @@
-const threeSumClosest = (nums, target) => {
-    nums = nums.sort()
-    let closestSum = 0
-    let minDistance = Number.MAX_SAFE_INTEGER
+// const threeSumClosest = (nums, target) => {
+//     nums = nums.sort()
+//     let closestSum = 0
+//     let minDistance = Number.MAX_SAFE_INTEGER
 
-    for(let i = 0;i < nums.length - 2;i++) {
-        let start = i + 1
-        let end = nums.length - 1
+//     for(let i = 0;i < nums.length - 2;i++) {
+//         let start = i + 1
+//         let end = nums.length - 1
 
-        while(start < end) {
-            let sum = nums[i] + nums[start] + nums[end]
-            let distance = Math.abs(target - sum)
-            console.log({sum,distance})
-            if(sum === target) {
-                closestSum = sum
-            }
+//         while(start < end) {
+//             let sum = nums[i] + nums[start] + nums[end]
+//             let distance = Math.abs(target - sum)
+//             console.log({sum,distance})
+//             if(sum === target) {
+//                 closestSum = sum
+//             }
             
-            if(distance < minDistance) {
-                minDistance = distance
-                closestSum = sum
-            }
+//             if(distance < minDistance) {
+//                 minDistance = distance
+//                 closestSum = sum
+//             }
 
-            if(sum < target) {
-                start++
-            }else {
-                end--
-            }
-        }
-    }
-    console.log(closestSum)
-    return closestSum
-};
+//             if(sum < target) {
+//                 start++
+//             }else {
+//                 end--
+//             }
+//         }
+//     }
+//     console.log(closestSum)
+//     return closestSum
+// };
 
-threeSumClosest([-4,-1,1,2],1)
+const threeSumClosest = (nums, target) => {
+    nums.sort((a,b) => {
+        return a - b
+    })
+    console.log(nums)
+}
+
+
+threeSumClosest([-1,2,1,-4],1)
