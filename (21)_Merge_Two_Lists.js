22ea0d6fa8da848b0e9d4047122f51a18f7cc1c6
Mike Tagg <mdtagg91@gmail.com>
2023-07-05 16:18:31 -0700

    merge two sorted linked list challenge solution added

--- (21)_Merge_Two_Lists.js
+++ (21)_Merge_Two_Lists.js
@@ -1,35 +1,38 @@
 
 
-var mergeTwoLists = function(list1, list2) {
-    
+var mergeTwoLists = function(l1, l2) {
+    if (!l1) return l2;
+    else if (!l2) return l1;
+    else if (l1.val <= l2.val) {
+        l1.next = mergeTwoLists(l1.next, l2);
+        return l1;
+    } else {
+        l2.next = mergeTwoLists(l1, l2.next);
+        return l2
+    }
 };
-
-// class LinkedList {
-//     constructor(head = null) {
-//         this.head = head
-//     }
-// }
-
-// class ListNode {
-//     constructor(data) {
-//         this.data = data 
-//         this.next = null
-//     }
-// }
 
 function ListNode(val, next) {
     this.val = (val===undefined ? 0 : val)
     this.next = (next===undefined ? null : next)
-
-    return val
 }
 
-const node3 = ListNode(4)
-const node2 = ListNode(2,node3)
-const node1 = ListNode(1,node2)
-const l1 = new ListNode([node1,node2,node3],node2)
+const node3 = new ListNode(4)
+const node2 = new ListNode(2,node3)
+const l1 = new ListNode(1,node2)
+// const l1 = new ListNode([node1,node2,node3],node2)
 
-console.log(l1.next)
+// console.log(l1)
+
+const node2_3 = new ListNode(4)
+const node2_2 = new ListNode(3,node2_3)
+const l2 = new ListNode(1,node2_2)
+
+console.log(mergeTwoLists(l1,l2))
+
+// console.log()
+
+
 // let node1 = new ListNode(1)
 // let node2 = new ListNode(2)
 // let node3 = new ListNode(4)
@@ -39,3 +42,19 @@ console.log(l1.next)
 
 // let list1 = new LinkedList(node1)
 // console.log(list1)
+
+// {
+//     val: { value: 1 },
+//     next: {
+//       value: [2,4],
+//     }
+//   }
+//   {
+//     val: { value: 2 },
+//     next: { value: [4] }
+//   }
+//   {
+//     val: { value: 4 },
+//     next: { value: null }
+//   }
+  
