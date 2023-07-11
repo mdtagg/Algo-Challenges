72865b62d6d6b9833e5e96907818c626f5ff1d70
Mike Tagg <mdtagg91@gmail.com>
2023-07-10 19:52:09 -0700

    15 3sum new solution added

--- leet_code/medium/(15)_3_Sum.js
+++ leet_code/medium/(15)_3_Sum.js
@@ -5,40 +5,32 @@ i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
 Notice that the solution set must not contain duplicate triplets.
 */
 
-
-
-
-
-
-
 var threeSum = function(nums) {
     nums = nums.sort((a,b) => a - b)
-    // console.log(nums)
-    let validSums = []
-    let validSumStrs = []
-    for(let i = 0;i < nums.length;i++) {
+    const triplets = []
+
+    for(let i = 0;i < nums.length - 2;i++) {
         let left = i + 1
         let right = nums.length - 1
+
         while(left < right) {
-            let potentialSum = [nums[i],nums[left],nums[right]]
-            // console.log(potentialSum)
-            let sum = nums[i] + nums[left] + nums[right]
-            if(sum > 0) {
-                right--
-            }else if(sum === 0 && !validSumStrs.includes(potentialSum.join(''))) {
-                validSums.push(potentialSum)
-                validSumStrs.push(potentialSum.join(''))
+            const sum = nums[i] + nums[left] + nums[right]
+            if(sum === 0) {
+                triplets.push([nums[i],nums[left],nums[right]])
+                while(nums[left] === nums[left + 1]) left++;
+            }
+            if(sum <= 0) {
                 left++
             }else {
-                left++
+                right--
             }
         }
-        if(i === nums.length - 3) break
+        while(nums[i] === nums[i + 1]) i++;
     }
-    console.log(validSums)
+    return triplets
 }
 
-threeSum([-1,0,1,2,-1,-4])
+threeSum([0,0,0,0])
 
 /*
 The key is to make the most of sorting prior to performing any operations. If we know that the 
@@ -49,6 +41,70 @@ We move the largest when the sum of our numbers is greater than 0. When the sum 
 0 it means that the lowest numbers left will not be able to produce a sum equal to zero, since 
 we are moving from smallest to largest. 
 */
+
+
+// for(let i = 0;i < nums.length - 2;i++) {
+    //     for(let j = i + 1;j < nums.length - 1;j++) {
+    //         console.log(nums[i],nums[j],nums[nums.length - 1])
+    //         const sum = nums[i] + nums[j] + nums[nums.length - 1]
+    //         if(sum === 0) {
+    //             triplets.push([nums[i],nums[j],nums[nums.length - 1]])
+    //         }
+    //     }
+    // }
+    // console.log(triplets)
+
+
+
+// nums = nums.sort((a,b) => a - b)
+    // // console.log(nums)
+    // let validSums = []
+    // let validSumStrs = []
+    // for(let i = 0;i < nums.length;i++) {
+    //     let left = i + 1
+    //     let right = nums.length - 1
+    //     while(left < right) {
+    //         let potentialSum = [nums[i],nums[left],nums[right]]
+    //         // console.log(potentialSum)
+    //         let sum = nums[i] + nums[left] + nums[right]
+    //         if(sum > 0) {
+    //             right--
+    //         }else if(sum === 0 && !validSumStrs.includes(potentialSum.join(''))) {
+    //             validSums.push(potentialSum)
+    //             validSumStrs.push(potentialSum.join(''))
+    //             left++
+    //         }else {
+    //             left++
+    //         }
+    //     }
+    //     if(i === nums.length - 3) break
+    // }
+    // console.log(validSums)
+
+
+
+// nums = nums.sort((a,b) => a - b)
+//     let validSums = []
+//     let validSumStrs = []
+//     for(let i = 0;i < nums.length;i++) {
+//         let left = i + 1
+//         let right = nums.length - 1
+//         while(left < right) {
+//             let potentialSum = [nums[i],nums[left],nums[right]]
+//             let sum = nums[i] + nums[left] + nums[right]
+//             if(sum > 0) {
+//                 right--
+//             }else if(sum === 0 && !validSumStrs.includes(potentialSum.join(''))) {
+//                 validSums.push(potentialSum)
+//                 validSumStrs.push(potentialSum.join(''))
+//                 left++
+//             }else {
+//                 left++
+//             }
+//         }
+//         if(i === nums.length - 3) break
+//     }
+//     return validSums
 
 
 // var threeSum = function(nums) {
