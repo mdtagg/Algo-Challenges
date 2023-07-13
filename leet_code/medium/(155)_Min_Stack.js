20bde2e93121a7209c9916e2d8f5d58096d0e0c8
Mike Tagg <mdtagg91@gmail.com>
2023-07-13 13:19:35 -0700

    155 min stack challenge solution added

---
+++ leet_code/medium/(155)_Min_Stack.js
@@ -0,0 +1,41 @@
+
+
+var MinStack = function() {
+    this.stack = []
+};
+
+MinStack.prototype.push = function(val) {
+    this.stack = [...this.stack,val]
+};
+
+MinStack.prototype.pop = function() {
+    // const lastValue = this.stack[this.stack.length - 1]
+    // this.stack = this.stack.slice(0,this.stack.length - 1)
+    // return lastValue
+    return this.stack.splice(this.stack.length - 1)
+};
+
+MinStack.prototype.top = function() {
+    return this.stack[this.stack.length - 1]
+};
+
+MinStack.prototype.getMin = function() {
+    let min = Infinity
+    this.stack.forEach(item => {
+        if(item < min) {
+            min = item
+        }
+    })
+    return min
+};
+
+const obj = new MinStack()
+obj.push(3)
+obj.push(5)
+obj.pop()
+obj.push(7)
+obj.getMin()
+const test = obj.top()
+const testTwo = obj.getMin()
+console.log({test,testTwo})
+console.log(obj)
