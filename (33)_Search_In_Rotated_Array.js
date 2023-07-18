3a652ab835fcb012c78daccd532e52ce911ff18c
Mike Tagg <mdtagg91@gmail.com>
2023-07-18 16:53:06 -0700

    33 search rotated array solution added

---
+++ (33)_Search_In_Rotated_Array.js
@@ -0,0 +1,81 @@
+/*
+
+There is an integer array nums sorted in ascending order
+ (with distinct values).
+
+Prior to being passed to your function, nums is possibly 
+rotated at an unknown pivot index k (1 <= k < nums.length)
+ such that the resulting array is [nums[k], nums[k+1], ...,
+  nums[n-1], nums[0], nums[1], ..., nums[k-1]] (0-indexed). 
+  For example, [0,1,2,4,5,6,7] might be rotated at pivot index 
+  3 and become [4,5,6,7,0,1,2].
+
+Given the array nums after the possible rotation and an integer
+ target, return the index of target if it is in nums, or -1 if 
+ it is not in nums.
+
+You must write an algorithm with O(log n) runtime complexity.
+
+*/
+
+var search = function(nums, target) {
+    let left = 0;
+    let right = nums.length - 1;
+      
+    while (left <= right) {
+      let mid = Math.floor((left + right) / 2);
+      
+      if (nums[mid] === target) {
+        return mid;
+      }
+      if (nums[left] <= nums[mid]) {
+        if (nums[left] <= target && target <= nums[mid]) {
+          right = mid - 1;
+          
+        } else {
+          left = mid + 1;
+        }
+      } 
+      else {
+        if (nums[mid] <= target && target <= nums[right]) {
+          left = mid + 1;
+  
+        } else {
+          right = mid - 1;
+        }
+      }
+    }
+    return -1;
+  };
+
+console.log(search([0,1,2,3,5],3))
+
+// if(nums.length === 1 && nums[0] === target) {
+//     return 0
+// }   
+// if(nums.length === 2) {
+//     return nums[0] === target ? 0 : nums[1] === target ? 1 : -1
+    
+// }
+// while(left < right) {
+//     const mid = Math.floor((left + right) / 2)
+//     if(nums[left] === target) {
+//         return left
+//     }else if(nums[mid] > target) {
+//         left = mid + 1
+//     }
+//     else {
+//         right = mid
+//     }
+// }
+// return -1
+
+// const mid = Math.floor((left + right) / 2)
+//         if(nums[left] === target) {
+//             return left
+//         }else if(nums[mid] > target) {
+//             left = mid + 1
+//         }
+//         else {
+//             right = mid
+//         }
