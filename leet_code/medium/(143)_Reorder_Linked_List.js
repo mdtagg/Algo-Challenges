25e53a65b41c2d5c04bcd13d25b53b517ed6c53a
Mike Tagg <mdtagg91@gmail.com>
2023-08-03 10:56:10 -0700

    143 reorder linked list solution added

--- leet_code/medium/(143)_Reorder_Linked_List.js
+++ leet_code/medium/(143)_Reorder_Linked_List.js
@@ -21,7 +21,35 @@ list and the second section.
  */
 
 const reorderList = (head) => {
-  
+  let slow = head 
+  let fast = head 
+
+  while(fast.next && fast.next.next) {
+    slow = slow.next 
+    fast = fast.next.next
+  }
+
+  let prev = null 
+  let curr = slow.next 
+
+  while(curr) {
+    let temp = curr.next
+    curr.next = prev
+    prev = curr
+    curr = temp
+  }
+
+  slow.next = null 
+
+  let h1 = head 
+  let h2 = prev 
+
+  while(h2) {
+    let temp = h1.next
+    h1.next = h2 
+    h1 = h2
+    h2 = temp
+  }
 } 
 
 reorderList(node1)
