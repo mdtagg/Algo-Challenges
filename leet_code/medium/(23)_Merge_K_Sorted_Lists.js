9a7571f5a1688d280e0ad3ee1816e2c99fc8b9e0
Mike Tagg <mdtagg91@gmail.com>
2023-08-11 12:02:09 -0700

    23 merge k lists redo solution added

--- leet_code/medium/(23)_Merge_K_Sorted_Lists.js
+++ leet_code/medium/(23)_Merge_K_Sorted_Lists.js
@@ -23,9 +23,25 @@ l2.next.next = new ListNode(4)
 const l3 = new ListNode(2)
 l3.next = new ListNode(6)
 
+function mergeTwoLists(l1,l2) {
+    if(!l1) return l2 
+    if(!l2) return l1
+
+    if(l1.val < l2.val) {
+        l1.next = mergeTwoLists(l1.next,l2)
+        return l1
+    }else {
+        l2.next = mergeTwoLists(l1,l2.next)
+        return l2
+    }
+}
 
 function mergeKLists(lists) {
-
+    let result = null 
+    for(let i = 0;i < lists.length;i++) {
+        result = mergeTwoLists(result,lists[i])
+    }
+    return result
 }
 
 mergeKLists([l1,l2,l3])
