6ed7e7dadcddc85b8d352e5e365e87958ffb0c51
Mike Tagg <mdtagg91@gmail.com>
2023-08-14 12:31:17 -0700

    110 balanced tree redo solution added

--- leet_code/easy/(110)_Balanced_Tree.js
+++ leet_code/easy/(110)_Balanced_Tree.js
@@ -1,3 +1,15 @@
+/*
+Given a binary tree, determine if it is 
+height-balanced
+
+(A height-balanced binary tree is a 
+binary tree in which the depth 
+of the two subtrees of every 
+node never differs by more than 
+one.)
+.
+*/
+
 function TreeNode(val,left,right) {
     this.val = (val===undefined ? 0 : val)
     this.left = (left===undefined ? null : left)
@@ -13,6 +25,23 @@ tree.left.right.left = new TreeNode(6)
 
 const isBalanced = (tree) => {
 
+    let balanced = true
+
+    function dfs(tree) {
+
+        if(!tree || !balanced) return 0
+
+        let left = dfs(tree.left)
+        let right = dfs(tree.right)
+
+        if(Math.abs(left - right) > 1) {
+            balanced = false
+        }
+        return Math.max(left,right) + 1
+
+    }
+
+    dfs(tree)
     
 }
 
