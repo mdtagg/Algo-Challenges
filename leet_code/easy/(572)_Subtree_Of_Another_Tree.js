28754a30b6f36342816df57c7a6f7475b0d86c66
Mike Tagg <mdtagg91@gmail.com>
2023-08-16 12:46:17 -0700

    572 subtree of tree redo solution added

--- leet_code/easy/(572)_Subtree_Of_Another_Tree.js
+++ leet_code/easy/(572)_Subtree_Of_Another_Tree.js
@@ -12,39 +12,76 @@ function TreeNode(val,left,right) {
 // root1.left.right = new TreeNode(2)
 // root1.left.right.left = new TreeNode(0)
 
-const root1 = new TreeNode(1)
-root1.left = new TreeNode(1)
-
 // const root2 = new TreeNode(4)
 // root2.left = new TreeNode(1) 
 // root2.right = new TreeNode(2)
+
+const root1 = new TreeNode(1)
+root1.left = new TreeNode(1)
 const root2 = new TreeNode(1)
 
-const isSubtree = (root, subRoot) => {
 
-    function isEqual(node1,node2) {
-        if(!node1 || !node2) return !node1 && !node2
-        if(node1.val !== node2.val) return false 
-        const leftEqual = isEqual(node1.left,node2.left)
-        const rightEqual = isEqual(node1.right,node2.right)
-        return leftEqual && rightEqual
-    }
+function sameRoot(root1,root2) {
+    if(!root1 && !root2) return true 
+    if((!root1 && root2) || (root1 && !root2)) return false 
+    if(root1.val !== root2.val) return false 
+    return sameRoot(root1.left,root2.left) && sameRoot(root1.right,root2.right)
+}
+
+function isSubTree(root,subroot) {
+    if(!subroot) return true 
+    if(!root) return false 
+    if(sameRoot(root,subroot)) return true
+    return isSubTree(root.left,subroot) || isSubTree(root.right,subroot)
+}
+    
+isSubtree(root1,root2)
+
+/*
+PERFORMANCE SOLUTION 
+
+function isSubtree(root, subRoot) {
+	if(!subRoot)
+		return true;
+	if(!root)
+		return false
+	if(isSameTree(root, subRoot))
+		return true;
+	return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
+}
+
+function isSameTree(root1,root2) {
+	if(!root1 && !root2)
+		return true;
+	if((!root1 && root2) || (root1 && !root2))
+		return false;
+	if(root1.val !== root2.val)
+		return false;
+	return isSameTree(root1.left, root2.left) && 
+		isSameTree(root1.right, root2.right);
+}
+
+
+INITIAL SOLUTION 
+
+function isEqual(root1,root2) {
+    if(!root1 || !root2) return !root1 && !root2 
+    if(root1.val !== root2.val) return false
+    return isEqual(root1.left,root2.left) && isEqual(root1.right,root2.right)
+}
+
+function isSubtree(root,subroot) {
     const queue = [root]
     while(queue.length) {
         const node = queue.pop()
         if(!node) continue 
-        if(isEqual(node,subRoot)) return true 
-        queue.push(node.left,node.right)
+        if(isEqual(node,subroot)) return true 
+        queue.push(node.right,node.left)
     }
     return false
 }
 
-isSubtree(root1,root2)
-
-/*
-
-
-
+CODE GRAVEYARD
 
 let match = true
 
