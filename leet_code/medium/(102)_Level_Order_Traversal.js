fd6c2f86c8c7fe23a503e00085e7171586f56bcb
Mike Tagg <mdtagg91@gmail.com>
2023-08-19 09:52:05 -0700

    102 binary tree level order traversal solution added

--- leet_code/medium/(102)_Level_Order_Traversal.js
+++ leet_code/medium/(102)_Level_Order_Traversal.js
@@ -13,40 +13,37 @@ function TreeNode(val,left,right) {
 // const root = new TreeNode(1)
 // root.left = new TreeNode(2)
 
-const root = new TreeNode(1)
+// const root = new TreeNode(1)
+// root.left = new TreeNode(2)
+// root.right = new TreeNode(3)
+// root.left.left = new TreeNode(4)
+// root.right.right = new TreeNode(5)
+
+const root = new TreeNode(0)
 root.left = new TreeNode(2)
-root.right = new TreeNode(3)
-root.left.left = new TreeNode(4)
-root.right.right = new TreeNode(5)
+root.left.left = new TreeNode(1)
+root.left.left.left = new TreeNode(5)
+root.left.left.right = new TreeNode(1)
+
+root.right = new TreeNode(4)
+root.right.left = new TreeNode(3)
+root.right.left.left = new TreeNode(6)
+root.right.right = new TreeNode(-1)
+root.right.right.right = new TreeNode(-6)
 
 var levelOrder = function(root) {
-    // If root is null return an empty array
     if(!root) return []
-    
-    const queue = [root] // initialize the queue with root
-    const levels = [] // declare output array
-    
-    while(queue.length !== 0){
-       const queueLength = queue.length // Get the length prior to dequeueing
-       const currLevel = [] // Declare this level
-       // loop through to exhaust all options and only to include nodes at currLevel
-       for(let i = 0; i < queueLength; i++){
-           // Get next node
-           const current = queue.shift()
-           
-           if(current.left){
-               queue.push(current.left)
-           }
-           if(current.right){
-               queue.push(current.right)
-           }
-           // After we add left and right for current, we add to currLevel
-           currLevel.push(current.val)
-       }
-       // Level has been finished. Push into output array
-       levels.push(currLevel)
-   }
-    return levels
+    const arr = []
+
+    function dfs(node,level = 0) {
+        if(!node) return null
+        if(!arr[level]) arr[level] = []
+        arr[level].push(node.val)
+        dfs(node.left,level + 1)
+        dfs(node.right,level + 1)
+    }
+    dfs(root)
+    return arr
 }
 
 levelOrder(root)
@@ -54,6 +51,23 @@ levelOrder(root)
 /*
 INITIAL SOLUTION
 
+if(!root) return []
+    const arr = []
+    let level = 0
+
+    function dfs(node,level) {
+        if(!node) return null
+        if(!arr[level]) arr.push([])
+        let left = dfs(node.left,level + 1)
+        let right = dfs(node.right,level + 1)
+        if(left || left === 0) arr[level+1].push(left)
+        if(right || right === 0) arr[level+1].push(right)
+        return node.val
+    }
+    let test = dfs(root,level)
+    arr[level].push(test)
+    return arr
+
 var levelOrder = function(root) {
     // If root is null return an empty array
     if(!root) return []
@@ -83,6 +97,23 @@ var levelOrder = function(root) {
    }
     return levels
 }
+
+if(!root) return []
+    const arr = []
+    let level = 0
+
+    function dfs(node,level) {
+        if(!node) return null
+        if(!arr[level]) arr.push([])
+        let left = dfs(node.left,level + 1)
+        let right = dfs(node.right,level + 1)
+        if(left) arr[level+1].unshift(left)
+        if(right) arr[level+1].push(right)
+        return node.val
+    }
+    let test = dfs(root,level)
+    arr[level].push(test)
+    return arr
 
 CODE GRAVEYARD
 
