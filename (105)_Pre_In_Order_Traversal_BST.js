80ecdf551c1fe73b1c02666856d6748e63b99498
Mike Tagg <mdtagg91@gmail.com>
2023-08-21 16:29:10 -0700

    105 BST preorder inorder creation solution added

---
+++ (105)_Pre_In_Order_Traversal_BST.js
@@ -0,0 +1,44 @@
+function TreeNode(val, left, right) {
+    this.val = (val===undefined ? 0 : val)
+    this.left = (left===undefined ? null : left)
+    this.right = (right===undefined ? null : right)
+}
+
+const pre1 = [3,9,20,15,7]
+const in1 = [9,3,15,20,7]
+
+var buildTree = function(preorder, inorder) {
+    p = i = 0
+    build = function(stop) {
+        if (inorder[i] != stop) {
+            var root = new TreeNode(preorder[p++])
+            root.left = build(root.val)
+            i++
+            root.right = build(stop)
+            return root
+        }
+        return null
+    }
+    return build()
+};
+
+buildTree(pre1,in1)
+
+/*
+CODE GRAVEYARD
+
+var buildTree = function(preorder, inorder) {
+    p = i = 0
+    build = function(stop) {
+        if (inorder[i] != stop) {
+            var root = new TreeNode(preorder[p++])
+            root.left = build(root.val)
+            i++
+            root.right = build(stop)
+            return root
+        }
+        return null
+    }
+    return build()
+};
+*/
