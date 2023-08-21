dec3c8ef65a18ac63370b213f56246fc69ea2513
Mike Tagg <mdtagg91@gmail.com>
2023-08-21 14:27:17 -0700

    230 kth smallest value BST solution added

---
+++ (230)_Kth_Smallest_Element_BST.js
@@ -0,0 +1,74 @@
+
+function TreeNode(val, left, right) {
+    this.val = (val===undefined ? 0 : val)
+    this.left = (left===undefined ? null : left)
+    this.right = (right===undefined ? null : right)
+}
+
+const root = new TreeNode(5)
+root.left = new TreeNode(3)
+root.right = new TreeNode(6)
+root.left.right = new TreeNode(4)
+root.left.left = new TreeNode(2)
+root.left.left.left = new TreeNode(1)
+
+const kthSmallest = (root,k) => {
+
+    let numbers = []
+    
+    function dfs(node) {
+        if(numbers.length === k) return
+        if(node.left) dfs(node.left)
+        numbers.push(node.val)
+        if(node.right) dfs(node.right)
+    }
+    dfs(root)
+    return numbers[k - 1]
+}
+
+kthSmallest(root,3)
+
+/*
+CODE GRAVEYARD
+
+let count = 0
+    let dummy = root
+    let min = root.val
+
+    while(count < k) {
+        if(dummy.left) dummy = dummy.left 
+        else if(dummy.right) dummy = dummy.right
+        min = Math.min(min,dummy.val)
+        count++
+        if(!dummy.left && !dummy.right) {
+            min = Math.min(min,root.val)
+            dummy = root.right
+            count++
+        }
+        count++
+    }
+    return min
+
+//     function dfs(node) {
+//         if(count === k) return
+//         if(node.left) dfs(node.left)
+//         if(node.right) dfs(node.right)
+//         numbers.push(node.val)
+//         count++
+//     }
+//     dfs(root)
+
+let numbers = []
+    let count = 0
+
+    while(count < k) {
+        while(node.left) {
+            node = node.left 
+            numbers.push(node.val)
+            count++
+        }
+        node = node.right
+        numbers.push(node.val)
+        count++
+    }
+*/
