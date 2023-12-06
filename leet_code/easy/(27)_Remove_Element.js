702dec8d9c7cf1620e13d1281606d866007ecac3
Mike Tagg <mdtagg91@gmail.com>
2023-12-06 14:55:41 -0800

    leetcode 27 rework

--- leet_code/easy/(27)_Remove_Element.js
+++ leet_code/easy/(27)_Remove_Element.js
@@ -1,26 +1,44 @@
-var removeElement = function(nums, val) {
+
+
+const removeElement = (nums,val) => {
     let k = 0
     for(let i = 0;i < nums.length;i++) {
-        nums[i] === val ? nums[i] = '_' : k++
-    }
-    nums.sort()
-    return k
-};
-
-
-//BEST SOLUTION
-
-var removeElement = function (nums, val) {
-    let po = 0;
-    for (let i = 0; i < nums.length; i++) {
-        if (nums[i] != val) {
-            nums[po] = nums[i];
-            po++;
+        if(nums[i] === val) {
+            nums[i] = -1
+            k++
         }
     }
-    console.log(nums)
-    return po;
-};
-
+    nums.sort((a,b) => b - a)
+}
 
 removeElement([3,2,2,3],3)
+
+
+
+
+// var removeElement = function(nums, val) {
+//     let k = 0
+//     for(let i = 0;i < nums.length;i++) {
+//         nums[i] === val ? nums[i] = '_' : k++
+//     }
+//     nums.sort()
+//     return k
+// };
+
+
+// //BEST SOLUTION
+
+// var removeElement = function (nums, val) {
+//     let po = 0;
+//     for (let i = 0; i < nums.length; i++) {
+//         if (nums[i] != val) {
+//             nums[po] = nums[i];
+//             po++;
+//         }
+//     }
+//     console.log(nums)
+//     return po;
+// };
+
+
+// removeElement([3,2,2,3],3)
