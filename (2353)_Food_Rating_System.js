a03ab28ae162fd2456e88ebbf70534229bbeeca3
Mike Tagg <mdtagg91@gmail.com>
2023-12-17 17:26:30 -0800

    2353 food rating system incorrect added

--- (2353)_Food_Rating_System.js
+++ (2353)_Food_Rating_System.js
@@ -16,10 +16,11 @@ const FoodRatings = function(food, cuisines,ratings) {
         }
 
         if(foodMap[cuisines[i]]["highest"][0] < ratings[i]) {
+            foodMap[cuisines[i]]["prevHighest"] = foodMap[cuisines[i]]["highest"]
             foodMap[cuisines[i]]["highest"] = [ratings[i],[food[i]]]
         }
         else if(foodMap[cuisines[i]]["highest"][0] === ratings[i]) {
-            foodMap[cuisines[i]]["highest"] = [ratings[i],[...foodMap[cuisines[i]]["highest"][1],food[i]]]
+            // foodMap[cuisines[i]]["highest"] = [ratings[i],[...foodMap[cuisines[i]]["highest"][1],food[i]]]
         }
     }
 }
@@ -29,16 +30,19 @@ FoodRatings.prototype.changeRating = function(food,newRating) {
 //issues is here. need to figure out what to do when value is lower
 
     let category = itemMap.get(food)
-    let foodRating = foodMap[category][food]
-    const { highestRating, highestFoods } = foodMap[category]["highest"]
+    // let foodRating = foodMap[category][food]
+    const [ highestRating, highestFoods ] = foodMap[category]["highest"]
 
-    foodRating = newRating
+    foodMap[category][food] = newRating
 
-    if(foodRating > highestRating) {
+    if(foodMap[category][food] > highestRating) {
+        foodMap[cuisines[i]]["prevHighest"] = foodMap[cuisines[i]]["highest"]
         foodMap[category]["highest"] = [newRating,[food]]
     }
-    else if(foodRating === highestRating) {
+    else if(foodMap[category][food] === highestRating) {
         foodMap[category]["highest"] = [newRating,[...highestFoods,food]]
+    }else {
+        foodMap[category[i]]["highest"] = prevHighest.pop()
     }
 
 }
@@ -59,11 +63,63 @@ const ratings = [9,12, 8, 15, 14, 7]
 
 const obj = new FoodRatings(foods,cuisines,ratings)
 
+obj.highestRated('japanese')
 obj.changeRating('sushi',16)
+obj.changeRating('sushi',1)
+obj.highestRated('japanese')
 obj.changeRating('ramen',16)
 console.log(obj.highestRated('japanese'))
 
 /*
+function FoodData(name, cuisine, rating) {
+    this.name = name;
+    this.cuisine = cuisine;
+    this.rating = rating;
+}
+
+class FoodRatings {
+
+    constructor(foods, cuisines, ratings) {
+        //Map<string, FoodData>
+        this.foodNameToFoodData = new Map();
+        this.cuisineNameToHeapifiedFoodData = new Map();
+
+        for (let i = 0; i < foods.length; ++i) {
+            let currentFood = new FoodData(foods[i], cuisines[i], ratings[i]);
+            this.foodNameToFoodData.set(foods[i], currentFood);
+            if (!this.cuisineNameToHeapifiedFoodData.has(cuisines[i])) {
+                this.cuisineNameToHeapifiedFoodData.set(cuisines[i],
+                        new MaxPriorityQueue({compare: (x, y) => (x.rating === y.rating) ? x.name.localeCompare(y.name) : (y.rating - x.rating)}));
+            }
+            this.cuisineNameToHeapifiedFoodData.get(cuisines[i]).enqueue(currentFood);
+        }
+    }
+
+    changeRating(food, newRating) {
+        let toUpdate = this.foodNameToFoodData.get(food);
+        let updated = new FoodData(toUpdate.name, toUpdate.cuisine, newRating);
+        this.foodNameToFoodData.set(food, updated);
+        this.cuisineNameToHeapifiedFoodData.get(toUpdate.cuisine).enqueue(updated);
+    }
+
+    highestRated(cuisine) {
+        let highestRatedFoodForCuisine = "";
+        while (true) {
+            let food = this.cuisineNameToHeapifiedFoodData.get(cuisine).front();
+            if (food.rating !== this.foodNameToFoodData.get(food.name).rating) {
+                this.cuisineNameToHeapifiedFoodData.get(cuisine).dequeue();
+            } else {
+                highestRatedFoodForCuisine = food.name;
+                break;
+            }
+        }
+        return highestRatedFoodForCuisine;
+    }
+}
+
+
+
+
 const FoodRatings = function(food, cuisines,ratings) {
     foodMap = {}
     itemMap = new Map()
