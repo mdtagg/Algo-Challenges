f90bd03a07c34b807924dcb9242dba43abd1bf25
Mike Tagg <mdtagg91@gmail.com>
2023-12-21 13:41:23 -0800

    code smith objOfMatches added

--- Code_Smith.js
+++ Code_Smith.js
@@ -1,17 +1,33 @@
 
-const union = (arrays) => {
-    const result = []
-    arrays = arrays.reduce((newArray,oldArray) => newArray.concat(oldArray))
-    for(let i = 0;i < arrays.length;i++) {
-        if(!result.includes(arrays[i])) result.push(arrays[i])
+const objOfMatches = (array1,array2,callback) => {
+    const newObj = {}
+    
+    for(let i = 0;i < array1.length;i++) {
+      const capped = callback(array1[i])
+      const nextElement = array2[i]
+      if(capped === nextElement) {
+        newObj[array1[i]] = nextElement
+      }
     }
-    return result
-}
+    return newObj
+  }
+objOfMatches()
 
-const arr1 = [5, 10, 15];
-const arr2 = [15, 88, 1, 5, 7];
-const arr3 = [100, 15, 10, 1, 5];
-union([arr1,arr2,arr3])
+
+
+// const union = (arrays) => {
+//     const result = []
+//     arrays = arrays.reduce((newArray,oldArray) => newArray.concat(oldArray))
+//     for(let i = 0;i < arrays.length;i++) {
+//         if(!result.includes(arrays[i])) result.push(arrays[i])
+//     }
+//     return result
+// }
+
+// const arr1 = [5, 10, 15];
+// const arr2 = [15, 88, 1, 5, 7];
+// const arr3 = [100, 15, 10, 1, 5];
+// union([arr1,arr2,arr3])
 
 // console.log(union([arr1, arr2, arr3])); // should log: [5, 10, 15, 88, 1, 7, 100]
 
