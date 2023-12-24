16e27177dc6007c9c3e661e755fd0b4e0e1d52aa
Mike Tagg <mdtagg91@gmail.com>
2023-12-24 11:05:42 -0800

    1758 min changes to binary string second solution added

--- leet_code/easy/(1758)_Minimum_Changes_Binary_String.js
+++ leet_code/easy/(1758)_Minimum_Changes_Binary_String.js
@@ -8,25 +8,19 @@ const minOperations = (str) => {
     arr2[0] = arr2[0] == "1" ? "0" : "1"
 
     for(let i = 0;i < str.length - 1;i++) {
-        result1 = checkStr(arr1,i,result1)
-        result2 = checkStr(arr2,i,result2)
-    }
-    return result1 > result2 ? result2 : result1
-}
-
-function checkStr(arr,i,result) {
-    if(arr[i + 1] == arr[i]) {
-        if(arr[i] == 1) {
-            arr[i + 1] = 0
-        }else {
-            arr[i + 1] = 1
+        if(arr1[i + 1] == arr1[i]) {
+            ++result1
+            arr1[i + 1] = arr1[i] == 1 ? 0 : 1
         }
-        return ++result
+        if(arr2[i + 1] == arr2[i]) {
+            ++result2   
+            arr2[i + 1] = arr2[i] == 1 ? 0 : 1
+        }
     }
-    return result
+    return Math.min(result1,result2)
 }
 
-minOperations("1111")
+minOperations("111000")
 
 /*
 
