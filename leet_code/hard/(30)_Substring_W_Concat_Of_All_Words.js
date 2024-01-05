77952f2b612bb13fe02249688b5fa8f4cc980296
Mike Tagg <mdtagg91@gmail.com>
2024-01-05 15:52:39 -0800

    leetcode 30 additonal solution

--- leet_code/hard/(30)_Substring_W_Concat_Of_All_Words.js
+++ leet_code/hard/(30)_Substring_W_Concat_Of_All_Words.js
@@ -1,48 +1,51 @@
 
 
-function findSubstring(s,words) {
+var findSubstring = function(s, words) {
+    let wordLength = words[0].length;
+    let windowSize = wordLength * words.length;
+    let wordCount = new Map();
+    let result = [];
 
-    let wordLen = words[0].length 
-    let left = 0
-    let right = wordLen
-    let result = []
-
-    let wordsMap = new Map()
-    for(let i = 0;i < words.length;i++) {
-        wordsMap.set(words[i],(wordsMap.get(words[i]) || 0) + 1)
+    // Initialize word counts
+    for (let word of words) {
+        wordCount.set(word, (wordCount.get(word) || 0) + 1);
     }
 
-    while(right <= s.length) {
-        let substr = s.substring(left,right)
-        if(words.includes(substr)) {
+    for (let i = 0; i < wordLength; i++) { // This loop is to offset the starting point
+        let left = i;
+        let right = i;
+        let currentCount = new Map();
 
-            if(words.length == 1) result.push(left);
+        while (right + wordLength <= s.length) {
+            let word = s.substring(right, right + wordLength);
+            right += wordLength;
 
-            let newMap = new Map(wordsMap)
-            let j = right 
-            let k = right + wordLen
-            newMap.set(substr,newMap.get(substr) - 1)
-            let amt = 1
+            if (wordCount.has(word)) {
+                currentCount.set(word, (currentCount.get(word) || 0) + 1);
 
-            while(k <= s.length) {
-                let substr = s.substring(j,k)
-                if(words.includes(substr)) {
-                    if(newMap.get(substr) === 0) break
-                    newMap.set(substr,newMap.get(substr) - 1)
-                    amt++
-                }else break
-                if(amt === words.length) result.push(left)
-                j += wordLen 
-                k += wordLen
-            } 
+                // Move left pointer to maintain the window size constraint
+                while (currentCount.get(word) > wordCount.get(word)) {
+                    let leftWord = s.substring(left, left + wordLength);
+                    currentCount.set(leftWord, currentCount.get(leftWord) - 1);
+                    left += wordLength;
+                }
+
+                // If the window size is the same as total length of words, record the result
+                if (right - left === windowSize) {
+                    result.push(left);
+                }
+            } else {
+                // If the word is not in words, reset counters and move left pointer
+                currentCount.clear();
+                left = right;
+            }
         }
-        left++
-        right++
     }
-    return result
-}
 
-findSubstring("a",["a"])
+    return result;
+};
+
+findSubstring("wordgoodgoodgoodbestword",["word","good","best","word"])
 
 /*
 function findSubstring(s,words) {
