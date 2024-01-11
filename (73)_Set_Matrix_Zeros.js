d6538180bc7b646971c4b03f20b905a40d7fce7d
Mike Tagg <mdtagg91@gmail.com>
2024-01-11 15:16:28 -0800

    73 set matrix zeros leet code solution added

--- (73)_Set_Matrix_Zeros.js
+++ (73)_Set_Matrix_Zeros.js
@@ -1,38 +1,76 @@
 
 
+var setZeroes = function(matrix) {
+    const row = matrix.length;
+    const col = matrix[0].length;
+    const dummyRow = new Array(row).fill(-1);
+    const dummyCol = new Array(col).fill(-1);
+    for(let i=0;i<row;i++){
+        for(let j=0;j<col;j++){
+            if(matrix[i][j]==0){
+                dummyRow[i] = 0;
+                dummyCol[j] = 0;
+            }
+        }
+    }
+    for(let i=0;i<row;i++){
+        for(let j=0;j<col;j++){
+            if(dummyRow[i] == 0 || dummyCol[j] == 0 ){
+                matrix[i][j]=0;
+            }
+        }
+    }
+};
+
+setZeroes(
+    [[0,1,2,0],[3,4,5,2],[1,3,1,5]]
+)
+
+/*
 const setZeroes = (matrix) => {
 
-    let visited = new Set()
+    let rowSet = new Set()
+    let colSet = new Set()
 
     for(let i = 0;i < matrix.length;i++) {
-        let zero = false
+
         for(let j = 0;j < matrix[0].length;j++) {
-            if(zero === true && matrix[i][j] !== 0) matrix[i][j] = 0
-            else if(matrix[i][j] === 0 && !visited.has(j)) {
+            if(matrix[i][j] === 0) {
+                if(rowSet.has(i) || colSet.has(j)) continue;
 
                 let row = 0
                 let col = j
-                visited.add(col)
+                colSet.add(col)
 
                 while(row < matrix.length) {
                     matrix[row][col] = 0
                     row++
                 }
                 col = 0
-                while(col < j) {
-                    matrix[i][col] = 0
+                rowSet.add(i)
+                while(col < matrix[i].length) {
+                    if(matrix[i][col] !== 0) {
+                        matrix[i][col] = 0
+                    }
+                    else if(!colSet.has(col)) {
+                        colSet.add(col)
+                        let row = 0
+
+                        while(row < matrix.length) {
+                            matrix[row][col] = 0
+                            row++
+                        }
+                        col++
+                        continue
+                    }
+                    col++
                 }
-                zero = true
             }
         }
     }
+    return matrix
 }
 
-setZeroes(
-    [[1,1,1],[1,0,1],[1,1,1]]
-)
-
-/*
 const setZeros = (matrix) => {
 
     let visited = new Map()
