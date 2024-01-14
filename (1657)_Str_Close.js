038b54b53c6b9c7f2eab10d989bb162e0030009b
Mike Tagg <mdtagg91@gmail.com>
2024-01-14 14:07:10 -0800

    1657 initial work in progress leetcode

--- (1657)_Str_Close.js
+++ (1657)_Str_Close.js
@@ -5,19 +5,24 @@ const closeStrings = (word1,word2) => {
     let wordOneArr = new Array(26).fill(0)
     let wordTwoArr = new Array(26).fill(0)
 
+    /*
+    [7,3,1...,10]
+    []
+     */
     for(let i = 0;i < word1.length;i++) {
         wordOneArr[word1.charCodeAt(i) - 97]++
         wordTwoArr[word2.charCodeAt(i) - 97]++
     }
 
     let letterMap = new Map()
+    wordTwoArr = wordTwoArr.filter((item) => item !== 0)
 
     for(let i = 0;i < 26;i++) {
         if(wordOneArr[i] === 0) continue;
         letterMap.set(wordOneArr[i],(letterMap.get(wordOneArr[i]) || 0) + 1)
     }
 
-    for(let i = 0;i < 26;i++) {
+    for(let i = 0;i < wordTwoArr.length;i++) {
         
         if(!letterMap.get(wordTwoArr[i])) {
             return false
@@ -26,10 +31,67 @@ const closeStrings = (word1,word2) => {
         }
     }
     return true
-    
 }
 
-closeStrings("abc","bca")
+closeStrings("uaz","ssx")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+const closeStrings = (word1,word2) => {
+    if(word1.length !== word2.length) return false
+    let wordOneArr = new Array(26).fill(0)
+    let wordTwoArr = new Array(26).fill(0)
+
+    for(let i = 0;i < word1.length;i++) {
+        wordOneArr[word1.charCodeAt(i) - 97]++
+        wordTwoArr[word2.charCodeAt(i) - 97]++
+    }
+
+    let letterMap = new Map()
+    wordTwoArr = wordTwoArr.filter((item) => item !== 0)
+
+    for(let i = 0;i < 26;i++) {
+        if(wordOneArr[i] === 0) continue;
+        letterMap.set(wordOneArr[i],(letterMap.get(wordOneArr[i]) || 0) + 1)
+    }
+
+    for(let i = 0;i < wordTwoArr.length;i++) {
+        
+        if(!letterMap.get(wordTwoArr[i])) {
+            return false
+        }else {
+            letterMap.set(wordTwoArr[i],letterMap.get(wordTwoArr[i]) - 1)
+        }
+    }
+    return true
+}
+ */
 
 
 
