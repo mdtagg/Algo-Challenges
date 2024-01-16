ebd3079f11b395da271d782261293dcae2185d52
Mike Tagg <mdtagg91@gmail.com>
2024-01-15 16:01:06 -0800

    128 longest consecutive integer redo added

--- leet_code/easy/(219)_Contains_Duplicate_2.js
+++ leet_code/easy/(219)_Contains_Duplicate_2.js
@@ -15,3 +15,18 @@ const containsNearbyDuplicate = (nums,k) => {
 }
 
 containsNearbyDuplicate([1,0,1,1],1)
+
+/*
+PSUEDO CODE
+
+delcare a variable numsMap that references an instance of the map object
+
+iterate through each number in the nums array
+    if our numsMap object does not have the current number make that number a key and set its value to the current index 
+    if our numsMap object does have the current number 
+        compare the absolute value of the last value set to the current number to k
+        if the absolute value is less than or equal to k return true 
+        if it is greater than k reset the value associated with the current number in our numMap object to the current index
+        
+if upon iterating through all the numbers in the nums array does not return true then return false
+*/
--- leet_code/medium/(128)_Longest_Consecutive.js
+++ leet_code/medium/(128)_Longest_Consecutive.js
@@ -1,6 +1,28 @@
 
 
 const longestConsecutive = (nums) => {
+
+    if(nums.length === 0) return 0
+    nums = nums.sort((a,b) => a - b)
+    let max = 1
+    let result = 0
+
+    for(let i = 0;i < nums.length - 1;i++) {
+        if(nums[i] + 1 === nums[i + 1]) {
+            max++;
+            result = Math.max(result,max)
+        }
+        else {
+            max = 1
+        }
+    }
+    return result
+}
+
+longestConsecutive([0,3,7,2,5,8,4,6,0,1])
+
+/*
+const longestConsecutive = (nums) => {
     nums = nums.sort((a,b) => a - b)
     let currentLongest = 1
     let longest = 0
@@ -20,5 +42,4 @@ const longestConsecutive = (nums) => {
     }
     return longest
 }
-
-longestConsecutive([1,2,0,1])
+*/
