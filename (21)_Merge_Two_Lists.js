49cf177bf3c39dc5ffd358454fc7632013ea8b79
Mike Tagg <mdtagg91@gmail.com>
2024-02-14 16:31:02 -0800

    21 merge two sorted lists redo added

--- (21)_Merge_Two_Lists.js
+++ (21)_Merge_Two_Lists.js
@@ -7,20 +7,6 @@ splicing together the nodes of the first two lists.
 Return the head of the merged linked list.
 */
 
-//redo 3
-var mergeTwoLists = function(l1, l2) {
-    if(!l1) return l2
-    else if(!l2) return l1
-    else if(l1.val <= l2.val) {
-        l1.next = mergeTwoLists(l1.next,l2)
-        return l1 
-    } else {
-        l2.next = mergeTwoLists(l1,l2.next) 
-        return l2
-    }
-    
-};
-
 function ListNode(val, next) {
     this.val = (val===undefined ? 0 : val)
     this.next = (next===undefined ? null : next)
@@ -34,12 +20,79 @@ const node2_3 = new ListNode(4)
 const node2_2 = new ListNode(3,node2_3)
 const l2 = new ListNode(1,node2_2)
 
+
+var mergeTwoLists = function(l1, l2) {
+    
+    let mover = new ListNode()
+    let pointer = mover 
+
+    while(l1 && l2) {
+        if(l1.val <= l2.val) {
+            mover.next = l1 
+            mover = mover.next
+            l1 = l1.next
+        }
+        else {
+            mover.next = l2 
+            mover = mover.next 
+            l2 = l2.next
+        }
+    }
+    if(!l1) mover.next = l2;
+    if(!l2) mover.next = l1;
+
+    return pointer.next
+};
+
+
+
 console.log(mergeTwoLists(l1,l2))
 
 // - In this challenge we are using recursion to merge the two lists
 // - We do this by redefining what the next value in the linked list is on each recursion. 
 // - The first equality comparison chooses what the head of the linked list will be and then 
 //  each recursion resets the value of .next until we hit the tail of the list. 
+
+/*
+if(!l1) return l2
+    else if(!l2) return l1
+    else if(l1.val <= l2.val) {
+        l1.next = mergeTwoLists(l1.next,l2)
+        return l1 
+    } else {
+        l2.next = mergeTwoLists(l1,l2.next) 
+        return l2
+    }
+*/
+
+//REDO 2 FAIL
+
+// let mover = new ListNode()
+//     let pointer = mover
+//     let flip = false
+
+//     while(l1 || l2) {
+
+//         let newNode = new ListNode()
+
+//         if(!flip) {
+//             if(l1) {
+//                 newNode.val = l1.val
+//                 l1 = l1.next
+//                 flip = true
+//             }
+//         }else if(flip) {
+//             if(l2) {
+//                 newNode.val = l2.val 
+//                 l2 = l2.next 
+//                 flip = false
+//             }
+//         }
+
+//         mover.next = newNode 
+//         mover = newNode
+//     }
+//     return pointer.next
 
 //REDO 1
 
